420a924f2407a75f013f758245e65ae8f12a4ce6
Roman Gushchik <adrianed.fabled@gmail.com>
2022-08-09 00:50:52 +0300

    create newProject

---
+++ DiplomProject_LiveScoreFootball/DiplomProject_LiveScoreFootball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiplomProject_LiveScoreFootball
+//
+//  Created by admin on 9.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
