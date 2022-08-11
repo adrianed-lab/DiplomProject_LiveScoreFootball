36b2da4e325b3a87f27b365cddabc14134c68a7a
Roman Gushchik <adrianed.fabled@gmail.com>
2022-08-11 10:05:05 +0300

    cheak result

--- DiplomProject_LiveScoreFootball/DiplomProject_LiveScoreFootball/ViewController.swift
+++ DiplomProject_LiveScoreFootball/DiplomProject_LiveScoreFootball/ViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var alamofireAPIProvider: RestAPIProviderProtocol!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        alamofireAPIProvider = AlamofireAPIProvider()
+        
+        alamofireAPIProvider.getCountries { result in
+            switch result {
+            case .success(let value):
+                print(value)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 
 
