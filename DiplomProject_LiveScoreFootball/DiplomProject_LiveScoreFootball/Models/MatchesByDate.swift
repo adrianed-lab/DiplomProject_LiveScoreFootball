079a3bc7a5e931162b2178c25e5ec0a0a6139918
Roman Gushchik <adrianed.fabled@gmail.com>
2022-08-11 21:05:09 +0300

    update Struct MatchesByDate

--- DiplomProject_LiveScoreFootball/DiplomProject_LiveScoreFootball/Models/MatchesByDate.swift
+++ DiplomProject_LiveScoreFootball/DiplomProject_LiveScoreFootball/Models/MatchesByDate.swift
@@ -74,35 +74,10 @@ struct TeamInfo: Codable {
     let name, id: String
 }
 
-struct TimeClassInfo: Codable, Hashable {
+struct TimeClassInfo: Codable {
     let scheduled: Int
-    let finish: JSONNull?
+    let finish: Int?
     let timezone: String
-    let start: JSONNull?
+    let start: Int?
 }
 
-
-class JSONNull: Codable, Hashable {
-
-    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
-        return true
-    }
-
-    public var hashValue: Int {
-        return 0
-    }
-
-    public init() {}
-
-    public required init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        if !container.decodeNil() {
-            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
-        }
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-        try container.encodeNil()
-    }
-}
