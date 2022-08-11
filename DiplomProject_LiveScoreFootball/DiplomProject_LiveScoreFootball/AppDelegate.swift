2a2b21147d007fb2b7ce744be61fe6e1d608a8d6
Roman Gushchik <adrianed.fabled@gmail.com>
2022-08-11 23:18:39 +0300

    update Constants, get APIkey

--- DiplomProject_LiveScoreFootball/DiplomProject_LiveScoreFootball/AppDelegate.swift
+++ DiplomProject_LiveScoreFootball/DiplomProject_LiveScoreFootball/AppDelegate.swift
@@ -13,7 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        if let valueAPIkey =  Bundle.main.infoDictionary?["API_KEY"] as? String {
+            Constants.valueAPIkey = valueAPIkey
+        }
         return true
     }
 
--- DiplomProject_LiveScoreFootball/DiplomProject_LiveScoreFootball/Constants.swift
+++ DiplomProject_LiveScoreFootball/DiplomProject_LiveScoreFootball/Constants.swift
@@ -11,14 +11,7 @@ import Alamofire
 
 struct Constants {
     
-    static var valueAPIkey: String {
-        var valueKey = String()
-        if let valueAPIkey =  Bundle.main.infoDictionary?["API_KEY"] as? String {
-            valueKey = valueAPIkey
-        }
-        return valueKey
-    }
-        
+    static var valueAPIkey: String!
     static let headers: HTTPHeaders = [
         "X-RapidAPI-Key" : valueAPIkey,
         "X-RapidAPI-Host" : "livescore-football.p.rapidapi.com"
