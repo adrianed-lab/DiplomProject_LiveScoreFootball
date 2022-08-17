e005f4d2d4c3ed592e699cd484ec46e35cc7a3b5
Roman Gushchik <adrianed.fabled@gmail.com>
2022-08-17 10:14:22 +0300

    add method "initialViewController"

--- DiplomProject_LiveScoreFootball/DiplomProject_LiveScoreFootball/Router.swift
+++ DiplomProject_LiveScoreFootball/DiplomProject_LiveScoreFootball/Router.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import UIKit
+protocol MainRouter {
+    var navigationController: UINavigationController? {get set}
+    var builder: BuilderProtocol? {get set}
+}
+
+protocol RouterProtocol: MainRouter {
+    func initialViewController()
+}
+
+class Router: RouterProtocol {
+    
+    var navigationController: UINavigationController?
+    var builder: BuilderProtocol?
+    
+    init(navigationController: UINavigationController, builder: BuilderProtocol) {
+        self.navigationController = navigationController
+        self.builder = builder
+    }
+    
+    func initialViewController() {
+        if let navigationController = navigationController {
+            guard let scoreViewController = builder?.createScoreModule(router: self) else {return}
+            navigationController.viewControllers = [scoreViewController]
+        }
+    }
+    
+    
+    
+    
+}
