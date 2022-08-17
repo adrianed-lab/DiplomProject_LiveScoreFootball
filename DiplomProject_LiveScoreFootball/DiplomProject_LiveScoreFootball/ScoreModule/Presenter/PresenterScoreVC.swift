ef2a7c6b17b6e069c5e6a2e0a264b56d712be414
Roman Gushchik <adrianed.fabled@gmail.com>
2022-08-17 10:09:44 +0300

    add method "getMatchesByDate"

--- DiplomProject_LiveScoreFootball/DiplomProject_LiveScoreFootball/ScoreModule/Presenter/PresenterScoreVC.swift
+++ DiplomProject_LiveScoreFootball/DiplomProject_LiveScoreFootball/ScoreModule/Presenter/PresenterScoreVC.swift
@@ -8,5 +8,40 @@
 import Foundation
 import UIKit
 
-protocol Presenter
+protocol PresenterScoreViewProtocol: AnyObject {
+    init(view: ScoreViewProtocol, apiProvider: RestAPIProviderProtocol, router: RouterProtocol)
+    func getMatchesByDate(date: String)
+    var matchesBydate: MatchesByDate? {get set}
+}
 
+class PresenterScoreView: PresenterScoreViewProtocol {
+    weak var view: ScoreViewProtocol!
+    var apiProvider: RestAPIProviderProtocol!
+    var matchesBydate: MatchesByDate?
+    var router: RouterProtocol?
+    
+    required init(view: ScoreViewProtocol, apiProvider: RestAPIProviderProtocol, router: RouterProtocol) {
+        self.view = view
+        self.apiProvider = apiProvider
+        self.router = router
+        getMatchesByDate(date: Constants.currentDate)
+    }
+
+    func getMatchesByDate(date: String) {
+        apiProvider.getMatchesByDate(date: date) { [weak self] result in
+            guard let self = self else {return}
+            switch result {
+            case .success(let value):
+                self.matchesBydate = value
+                self.view.successGetMatchesByDate()
+            case .failure(let error):
+                self.view.failure(error: error)
+            }
+        }
+    
+    
+    
+    
+}
+
+}
