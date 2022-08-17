0fe12423e2b6fcabbf8d5d4d55269f7568fba10c
Roman Gushchik <adrianed.fabled@gmail.com>
2022-08-17 10:13:25 +0300

    add method "createScoreModule"

--- DiplomProject_LiveScoreFootball/DiplomProject_LiveScoreFootball/ModuleBuilder.swift
+++ DiplomProject_LiveScoreFootball/DiplomProject_LiveScoreFootball/ModuleBuilder.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import UIKit
+
+protocol BuilderProtocol {
+    func createScoreModule(router: RouterProtocol) -> UIViewController
+}
+
+class ModuleBuilder: BuilderProtocol {
+    func createScoreModule(router: RouterProtocol) -> UIViewController {
+        let view = ScoreViewController()
+        let apiProvider = AlamofireAPIProvider()
+        let presenter = PresenterScoreView(view: view, apiProvider: apiProvider, router: router)
+        view.presenter = presenter
+        return view
+    }
+    
+    
+}
