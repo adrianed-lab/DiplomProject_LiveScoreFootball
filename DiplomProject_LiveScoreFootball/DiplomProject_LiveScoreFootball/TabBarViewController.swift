ba493bcd86cf6c2366dd71151a5629f5eca22b17
Roman Gushchik <adrianed.fabled@gmail.com>
2022-08-22 12:28:26 +0300

    create TabBarVC

---
+++ DiplomProject_LiveScoreFootball/DiplomProject_LiveScoreFootball/TabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarViewController.swift
+//  DiplomProject_LiveScoreFootball
+//
+//  Created by admin on 21.08.22.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.tintColor = .red
+        tabBar.unselectedItemTintColor = .black
+    
+    }
+}
