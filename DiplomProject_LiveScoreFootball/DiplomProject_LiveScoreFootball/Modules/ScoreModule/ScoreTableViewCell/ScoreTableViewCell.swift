3306cc1955cb592e715b2763d0a351037e191cab
Roman Gushchik <adrianed.fabled@gmail.com>
2022-09-11 20:40:26 +0300

    update Cells

--- DiplomProject_LiveScoreFootball/DiplomProject_LiveScoreFootball/Modules/ScoreModule/ScoreTableViewCell/ScoreTableViewCell.swift
+++ DiplomProject_LiveScoreFootball/DiplomProject_LiveScoreFootball/Modules/ScoreModule/ScoreTableViewCell/ScoreTableViewCell.swift
@@ -9,14 +9,18 @@ import UIKit
 
 protocol ScoreTableViewCellProtocol: AnyObject {
     
-    func configureCell(codeCountry: String, countryNameInfo: String, leagueName: String)
+    func configureCell(date: String, lofoFirstTeam: Int, logoSecondTeam: Int, nameFirstTeam: String, nameSecondTeam: String, goalsHome: Int, goalsAway: Int)
     }
 
 class ScoreTableViewCell: UITableViewCell, ScoreTableViewCellProtocol {
     
-    @IBOutlet weak var countryName: UILabel!
-    @IBOutlet weak var leagueOrCupName: UILabel!
-    @IBOutlet weak var countryFlag: UIImageView!
+    @IBOutlet weak var matchDate: UILabel!
+    @IBOutlet weak var teamFirstLogo: UIImageView!
+    @IBOutlet weak var teamSecondLogo: UIImageView!
+    @IBOutlet weak var teamFirstName: UILabel!
+    @IBOutlet weak var teamSecondName: UILabel!
+    @IBOutlet weak var homeGoals: UILabel!
+    @IBOutlet weak var awayGoals: UILabel!
     static let key = "ScoreTableViewCell"
     
     override func awakeFromNib() {
@@ -28,9 +32,13 @@ class ScoreTableViewCell: UITableViewCell, ScoreTableViewCellProtocol {
 
     }
     
-    func configureCell(codeCountry: String, countryNameInfo: String, leagueName: String) {
-        countryName.text = countryNameInfo
-//        countryFlag.image =
-        leagueOrCupName.text = leagueName
+    func configureCell(date: String, lofoFirstTeam: Int, logoSecondTeam: Int, nameFirstTeam: String, nameSecondTeam: String, goalsHome: Int, goalsAway: Int) {
+        matchDate.text = date
+        teamFirstLogo.getLogoTeam(teamId: lofoFirstTeam)
+        teamSecondLogo.getLogoTeam(teamId: logoSecondTeam)
+        teamFirstName.text = nameFirstTeam
+        teamSecondName.text = nameSecondTeam
+        homeGoals.text = "\(goalsHome)"
+        awayGoals.text = "\(goalsAway)"
     }
 }
